b748ede10e1f196356d5d81e04afd0abf6fd3fa9
shubhiscoding <mygitcoin@gmail.com>
2026-03-20 00:29:20 +0530

    Update dependencies in Cargo.toml and Cargo.lock; enhance error handling in current and init commands

--- src/commands/current.rs
+++ src/commands/current.rs
@@ -1,9 +1,25 @@
 use std::{fs};
+use std::env;
 
 use crate::commands::print_ln;
 pub fn current_env(){
     let content = fs::read_to_string(".kakashi/config")
-        .expect("Run `kakashi init <path>` first");
+        .expect("Run `kakashi init <path>` first, or move to the correct directory");
+
+    let curr_path = env::current_dir().unwrap().display().to_string();
+    
+    if content.is_empty() {
+        println!("Please Run `kakashi init <path>` or move to the correct path");
+    }
+    
+    for line in content.lines() {
+        if line.starts_with("project_path=") {
+            let saved_path = line.replace("project_path=", "");
+            if saved_path != curr_path {
+                panic!("Please run init or move to the correct path");
+            }
+        }
+    }
     
     for line in content.lines() {
         if line.starts_with("current_env=") {
--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,23 +1,50 @@
 use std::fs::{self};
 use std::io::Write;
-
-use crate::commands::list_envs;
+use std::env;
+use std::path::Path;
+use crate::commands::{list_envs, print_ln};
 use crate::commands::print_kakashi;
 pub fn init_kakashi(path: &str){
-    fs::create_dir_all(".kakashi").unwrap();
+    let path = path.trim_matches(|c| c == '/' || c == '\\');
+    let curr_path = env::current_dir().unwrap();
+    let project_path = curr_path.join(Path::new(path));
+    let kakashi_dir = format!("{}/.kakashi", path);
+    fs::create_dir_all(&kakashi_dir).unwrap();
 
-    let mut config = fs::File::create(".kakashi/config").unwrap();
+    let mut config = fs::File::create(format!("{}/config", kakashi_dir)).unwrap();
 
-    writeln!(config, "env_dir={}", path).unwrap();
+    writeln!(config, "env_dir={}", ".").unwrap();
+    writeln!(config, "project_path={}", project_path.display()).unwrap();
+
     print_kakashi();
-    println!("Initialized kakashi with env path: {}", path);
-
-    list_envs();
+    print_ln();
+    if path != "."{
+        println!("Initialized kakashi with env path: {}", project_path.display());
+        println!("Run cd {}", path);
+    }else {
+        println!("Initialized kakashi with env path: {}", curr_path.display());
+    }
+    print_ln();
 }
 
 pub fn get_env_dir(envs: &str) -> String{
     let content = fs::read_to_string(".kakashi/config")
-        .expect("Run `kakashi init <path>` first");
+        .expect("Run `kakashi init <path>` first, or move to the correct directory");
+
+    let curr_path = env::current_dir().unwrap().display().to_string();
+    
+    if content.is_empty() {
+        println!("Please Run `kakashi init <path>` or move to the correct path");
+    }
+    
+    for line in content.lines() {
+        if line.starts_with("project_path=") {
+            let saved_path = line.replace("project_path=", "");
+            if saved_path != curr_path {
+                panic!("Please run init or move to the correct path");
+            }
+        }
+    }
 
     for line in content.lines() {
         if line.starts_with("env_dir=") {
